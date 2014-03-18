95d463845bc6ed72c2b6354f1772a5b8b36d08d7
Erik Steigler <erik.github@sublevo.com>
2014-03-18 13:00:25 -0400

    reinstate these

--- install.sh
+++ install.sh
@@ -24,9 +24,9 @@ else
     done
 fi
 
-#git submodule sync
-#git submodule init
-#git submodule update
-#git submodule foreach git pull origin master
-#git submodule foreach git submodule init
-#git submodule foreach git submodule update
+git submodule sync
+git submodule init
+git submodule update
+git submodule foreach git pull origin master
+git submodule foreach git submodule init
+git submodule foreach git submodule update
