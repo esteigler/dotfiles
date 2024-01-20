a84b90d0c0dc61abd306ac847ac11f76c240fba5
Erik Steigler <esteigler@users.noreply.github.com>
2024-01-20 11:55:07 -0500

    Add nvm to bashrc

--- bash.d/osx.bash
+++ bash.d/osx.bash
@@ -29,3 +29,7 @@ fi
 if [ -f ~/.venv/bin/activate ]; then
   source ~/.venv/bin/activate
 fi
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
